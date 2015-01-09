e6541556992b84488d0101f756794abb0d9cf3d7
Girish <girishramnani95@gmail.com>
2015-01-09 13:13:25 +0530

    Initial commit

---
+++ SystemTimeChanger/timeGrabber.py
@@ -0,0 +1,10 @@
+__author__ = 'Girish'
+
+import requests
+import bs4
+def grab_time_and_date():
+    bs = bs4.BeautifulSoup(requests.get("http://www.worldtimeserver.com/current_time_in_IN.aspx").content)
+    date =bs.find("span",{'class':'font6'}).text.strip()
+    time = bs.find("span",{'class':'font7'}).text.strip()
+    return time,date
+
