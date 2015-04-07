3a5081d235199d71a2251f0221b4fd8faef248af
girishramnani <girishramnani95@gmail.com>
2015-04-08 02:05:06 +0530

    compeleting the translator

--- googleTranslate/Subtitle_translator.py
+++ googleTranslate/Subtitle_translator.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import itertools
 from googleTranslate.Translator import Translator
 import re
 
@@ -7,8 +9,22 @@ class Subtitle_translator(Translator):
 
     def __init__(self,to,file):
         Translator.__init__(self,to)
-        self.re = re.compile(r' .... (?P<text>.*?)(\n\n|$)')
 
+
+    def set_progress_bar(self,progress_bar):
+        self.progress_bar = progress_bar
+
+    def translate_file(self,from_,to):
+        with open(to,'w') as out:
+            with open(from_) as file:
+                self.li= [ list(g) for f,g in itertools.groupby(file,key=lambda x:bool(x.strip())) if f]
+            for block in self.li:
+
+                line,time ,*content = block
+                out.writelines([line,time])
+                content =reduce(lambda x,y:" ".list(x.strip(),y.strip()),content,"")
+                data = Translator.translate(content)
+                out.write(content)
 
 
 
--- googleTranslate/other_tests.py
+++ googleTranslate/other_tests.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from googleTranslate import Translator
 
 __author__ = 'Girish'
@@ -15,8 +16,18 @@ class MyTestCase(unittest.TestCase):
         re1 = re.compile(r' .... (?P<text>.*?)(\n\n|$)')
         file2=open("test2.srt","w")
         with open("test.srt") as file:
-            self.li = [list(g) for f,g in itertools.groupby(file,key=lambda x:bool(x.strip())) if f]
-        print(self.li)
+            self.li= [ list(g) for f,g in itertools.groupby(file,key=lambda x:bool(x.strip())) if f]
+        for block in self.li:
+
+            line,time ,*content = block
+            file2.writelines([line,time])
+            content =reduce(lambda x,y:x.strip()+" "+y.strip(),content,"")
+            data = translator.translate(content)
+            print(data)
+
+            
+
+
 
 
 
