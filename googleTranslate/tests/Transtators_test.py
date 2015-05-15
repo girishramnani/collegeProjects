6502e61ad34d84afe1a5811e63f54f88cc8f1c9e
girishramnani <girishramnani95@gmail.com>
2015-05-15 14:23:45 +0650

    changed file googleTranslate/tests/Transtators_test.py

--- googleTranslate/tests/Transtators_test.py
+++ googleTranslate/tests/Transtators_test.py
@@ -1,3 +1,5 @@
+from googleTranslate.Subtitle_translator import Subtitle_translator
+
 __author__ = 'Girish'
 
 import unittest
@@ -6,6 +8,10 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
+    def test_translator(self):
+        Subtitle = Subtitle_translator("fr")
+        Subtitle.translate_file("test.srt","test2.srt")
+
 
 
 if __name__ == '__main__':
