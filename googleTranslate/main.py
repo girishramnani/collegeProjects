64379cd1681e299f416f66d5c91c5bfa2df23098
girishramnani <girishramnani95@gmail.com>
2015-05-15 14:23:45 +0650

    changed file googleTranslate/main.py

--- googleTranslate/main.py
+++ googleTranslate/main.py
@@ -1,7 +1,11 @@
+from tkinter import messagebox
+from tkinter.messagebox import showwarning
+from tkinter.filedialog import askopenfilename
 __author__ = 'Girish'
 import os
 from tkinter import *
 import tkinter.ttk as ttk
+
 
 class UI:
     def __init__(self,master):
@@ -14,24 +18,39 @@ class UI:
     def init_UI(self):
         self.label = Label(self.master,text="FileName:")
         self.entry = Entry(self.master)
-        self.button = Button(self.master,text="Select file")
+        self.button = Button(self.master,text="Select file",command=self.ask)
+        self.button2 =Button(self.master,text="Start",command=self.show_progress_bar)
+        self.button2.config(state="disabled")
         self.label.grid(row=0,column=0,pady=(5,5),padx=(10))
         self.entry.grid(row=0,column=1,pady=(5,5),padx=(10))
         self.button.grid(row=0,column=2,pady=(5,5),padx=(2))
+        self.button2.grid(row=0,column=3,padx=(2,5))
 
+
+
+    def ask(self):
+        self.file =askopenfilename(filetypes=(("Subtitle files", "*.srt"),
+                                            ))
+        print(self.file)
+        if self.file:
+            self.button2.config(state="normal")
+        else:
+            pass
 
 
 
     def show_progress_bar(self):
-        self.popup = popup = Toplevel(self)
-        Label(popup, text="Please while translation is being done").grid(
+        self.popup = Toplevel(self.master)
+        Label(self.popup, text="Please while translation is being done").grid(
             row=0, column=0)
-        self.progressbar = progressbar = ttk.Progressbar(popup,
+        self.progressbar = progressbar = ttk.Progressbar(self.popup,
             orient=HORIZONTAL, length=200, mode='indeterminate')
         progressbar.grid(row=1, column=0)
         progressbar.start()
         self.popup.attributes("-topmost",True)
 
+    def start_translating(self):
+        pass
 
 
 
